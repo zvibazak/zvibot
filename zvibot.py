62fb1f8a2eccc58819c8f34ab2e701660ffa1985
zvibazak <zvibazak@gmail.com>
2021-02-21 16:34:13 +0200

    Update zvibot.py

--- zvibot.py
+++ zvibot.py
@@ -3,8 +3,9 @@ import requests
 
 bot_token = os.getenv('BOT_TOKEN')
 bot_chatID = os.getenv('BOT_CHAT_ID')
-print(bot_token[:5])
 def telegram_bot_sendtext(bot_message,bot_chatID,bot_token):
+    print(bot_token[:5])
+    print(bot_chatID[:5])
     url = "https://api.telegram.org/bot"+bot_token+"/sendMessage"
     send_text = url + '?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     r = requests.get(send_text)
